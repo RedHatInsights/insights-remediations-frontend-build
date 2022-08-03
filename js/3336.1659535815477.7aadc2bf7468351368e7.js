e0d88ec7153b9e12af82385a8e8be27e8013373f
Travis CI User <travis@example.org>
2022-08-03 14:11:40 +0000

    Merge pull request #322 from RedHatInsights/master
    Releases master into production

---
+++ js/3336.1659535815477.7aadc2bf7468351368e7.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinsights_remediations_frontend=self.webpackChunkinsights_remediations_frontend||[]).push([[3336],{53336:()=>{}}]);
---
+++ js/6928.1659535815477.7aadc2bf7468351368e7.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinsights_remediations_frontend=self.webpackChunkinsights_remediations_frontend||[]).push([[6928],{46928:()=>{}}]);
